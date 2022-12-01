968b184432e58b5f9f97ae0b6e7c9db3b8d63b5b
George Herbert <github@georgeherby.dev>
2022-12-01 12:06:28 +0000

    chore: project setup

---
+++ src/content-scripts/modules/print.ts
@@ -0,0 +1,6 @@
+export const printLine = (message: String) => {
+    //TODO Make version dynamics
+    console.log("%c[DTX Plus v" + "0.0.1" + "]%c: " + message,
+        "font-size: 14px; color: #88f", "font-size: 14px; color: #fff");
+};
+
